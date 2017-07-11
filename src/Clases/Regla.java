15ba4a7df730d2932427aa3f210c3430b3feae46
rodrigoLaptop <raqb_350@hotmail.com>
2017-07-10 23:28:12 -0400

    Prmer Commit
    Primer avance para materia de IA

---
+++ src/Clases/Regla.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Clases;
+
+/**
+ *
+ * @author Rodrigo AB
+ */
+public class Regla {
+    public int fil;
+    public int col;
+
+    public Regla(int fil, int col) {
+        this.fil = fil;
+        this.col = col;
+    }
+
+    
+}
